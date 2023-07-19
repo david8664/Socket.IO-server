a198f51e3702751f6df8ee733fe8729e11942988
Yehuda Yeger <yeger921@gmail.com>
2023-07-19 17:42:52 +0300

    server update

--- index.js
+++ index.js
@@ -8,12 +8,27 @@ const httpServer = createServer(app);
 const io = new Server (httpServer)
 app.use (cors())
 
-io.on("connection", (socket) => {
+
+io.on("connection", (socket)  => {
   console.log ("User Connected")
-  socket.on ('disconnect', ()=> {
+socket.emit('start',"User Connected")
+socket.on("username",(msg)=> {
+    const users = msg    
+    console.log (users)
+    socket.broadcast.emit(`${users} connected`)
+})
+socket.on('uploadMessage',(msg)=> {
+socket.broadcast.emit ('downloadMessage',msg)
+console.log (msg)
+})
+  })
+
+  io.on ('disconnect', ()=> {
     console.log ("User Disconnected")
   })
-});
+
+
+
 
 
 
